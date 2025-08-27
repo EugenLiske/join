97c85fae598aa7f56812273dd8174896005ab93a
Anne <an-san@gmx.de>
2025-08-27 17:05:08 +0200

    clean - add task: function "setPriorityButtonDefault" shortened to 14 lines

--- add_task/form_selection/priority.js
+++ add_task/form_selection/priority.js
@@ -16,8 +16,16 @@ function setGlobalPriority(element, priority){
 
 
 function setPriorityButtonDefault(buttons, mode = "all"){
-    let id = -1;
+    setAllButtonInactive(buttons);
 
+    if (mode == "default"){
+        setMediumButtonDefault();
+    }
+}
+
+
+function setAllButtonInactive(buttons){
+    let id = -1;
     for (let buttonIdx = 0; buttonIdx < buttons.length; buttonIdx++) {
         id = buttons[buttonIdx].id;
 
@@ -26,13 +34,14 @@ function setPriorityButtonDefault(buttons, mode = "all"){
         buttons[buttonIdx].classList.replace(id + "_color_click", id + "_color_default");
         buttons[buttonIdx].classList.remove("priority_button_shadow_click");
         buttons[buttonIdx].children[0].classList.add("priority_btn_c_txt_default");
-    }
+    }    
+}
 
-    if (mode == "default"){
-        document.getElementById("medium").classList.replace("medium_color_default", "medium_color_click");
-        document.getElementById("medium").children[0].classList.remove("priority_btn_c_txt_default");
-        currentPriority = "medium";
-    }
+
+function setMediumButtonDefault(){
+    document.getElementById("medium").classList.replace("medium_color_default", "medium_color_click");
+    document.getElementById("medium").children[0].classList.remove("priority_btn_c_txt_default");
+    currentPriority = "medium";
 }
 
 
