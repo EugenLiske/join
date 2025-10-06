e6655cf6ca24e171c8ed2f949872829006cb89ac
Anne <an-san@gmx.de>
2025-10-06 15:41:54 +0200

    clean: move function "getContacts" to task_db_connection.js and delete "person_db.js" file

--- scripts/task_db_connection.js
+++ scripts/task_db_connection.js
@@ -76,3 +76,20 @@ async function deleteTaskFromFirebase(taskId) {
         throw error;
     }
 }
+
+
+async function getContacts() {
+    try {
+        const response = await fetch(`https://join-476d1-default-rtdb.firebaseio.com/contacts/data.json?t=${Date.now()}`);
+
+        if (!response.ok) {
+            throw new Error("Failed to load contacts");
+        }
+
+        return await response.json();
+
+    } catch (error) {
+        console.error("Error loading contacts:", error);
+    }
+    return null;
+}
